67612f6b98895b02d782400e61f739bc8027d1d0
PRANAV SHILU <pranavshilu1811@gmail.com>
2026-03-30 09:09:27 +0530

    variable explaination

--- DFS 2/GRAPH.C
+++ DFS 2/GRAPH.C
@@ -1,65 +1,68 @@
-#include<stdio.h>
-#include<conio.h>
-#define SOURCE 0
+#include <stdio.h>
+#include <conio.h>
+#define SOURCE 0 // source vertex for DFS and BFS to start from
 #define SIZE 4
-/* graph
+/* graph visualization
   0-----1
   |     |
   |     |
   3     2
 */
-int visited[SIZE]={0,0,0,0};
-int arr[SIZE][SIZE]={
-		{0,1,0,1},
-		{1,0,1,0},
-		{0,1,0,0},
-		{1,0,0,0}
-	      };
-int queue[SIZE],front=0,rear=0;
-int node;
+int visited[SIZE] = {0, 0, 0, 0}; // for checking wheter that node is visited or not
+int arr[SIZE][SIZE] = {
+	{0, 1, 0, 1},
+	{1, 0, 1, 0},
+	{0, 1, 0, 0},
+	{1, 0, 0, 0}};
+int queue[SIZE], front = 0, rear = 0; // for BFS,
+// queue is used to store the vertices which are visited and to be displayed
+// front is used to delete and display verted which came first
+// rear is used to insert the visited vertex in queue
+int node; // for storing the vertex which is being displayed in BFS
+
 void dfs(int cur)
 {
 	int i;
-	if(cur >= SIZE)
+	if (cur >= SIZE)
 	{
 		printf("Invalid source!");
 		return;
 	}
 
-	printf("%d -> ",cur);
-	visited[cur]=1;
+	printf("%d -> ", cur);
+	visited[cur] = 1;
 
-	for(i=0;i<SIZE;i++)
+	for (i = 0; i < SIZE; i++)
 	{
-		if(arr[cur][i]==1 && visited[i]==0)
+		if (arr[cur][i] == 1 && visited[i] == 0) // if there is an edge in adjacency vertex and that vertex is not visited
 			dfs(i);
 	}
 }
 void bfs(int start)
 {
 	int i;
-	if(start >= SIZE)
+	if (start >= SIZE)
 	{
 		printf("Invalid source!");
 		return;
 	}
 
-	visited[start]=1;
-	queue[rear]=start;
+	visited[start] = 1;
+	queue[rear] = start;
 	rear++;
 
-	while(front < rear)
+	while (front < rear)
 	{
-		node=queue[front];
+		node = queue[front];
 		front++;
-		printf("%d -> ",node);
+		printf("%d -> ", node);
 
-		for(i=0; i<SIZE; i++)
+		for (i = 0; i < SIZE; i++)
 		{
-			if(arr[node][i]==1 && visited[i]==0)
+			if (arr[node][i] == 1 && visited[i] == 0)
 			{
-				visited[i]=1;
-				queue[rear]=i;
+				visited[i] = 1;
+				queue[rear] = i;
 				rear++;
 			}
 		}
@@ -70,11 +73,13 @@ void main()
 	int ch;
 	clrscr();
 
+	// currently visited array is all 0, which means all vertex are unvisited, if we want to use BFS after DFS then we have to make all vertex unvisited again by making all element of visited array 0 again
+
 	printf("DFS:  ");
 	dfs(SOURCE);
 
 	printf("\n\nBFS:  ");
-	//bfs(SOURCE);
+	// bfs(SOURCE);
 
 	getch();
 }
