a9e5652798de677ebbfa6a479faf6c959b6a0966
PRANAV SHILU <pranavshilu1811@gmail.com>
2026-04-01 09:47:20 +0530

    refactor formatting and improve code readability in C-QUEUE implementation

--- DFS/14 C-QUEUE.C
+++ DFS/14 C-QUEUE.C
@@ -1,48 +1,50 @@
-#include<stdio.h>
-#include<conio.h>
-int arr[5],size=5,f=-1,r=-1,i;
+#include <stdio.h>
+#include <conio.h>
+int arr[5], size = 5, f = -1, r = -1, i;
+// f=front to delete
+// r=rear to insert
 void insert()
 {
-	if( (f==0 && r==size-1) || (f==r+1) )
+	if ((f == 0 && r == size - 1) || (f == r + 1))
 		printf("\nfull\n");
 	else
 	{
-		if(f==-1)
-			f=r=0;
-		else if(r==size-1)
-			r=0;
+		if (f == -1)
+			f = r = 0; // first time insertion
+		else if (r == size - 1)
+			r = 0;
 		else
 			r++;
 
-		scanf("%d",&arr[r]);
+		scanf("%d", &arr[r]);
 	}
 }
-void delete()
+void delete ()
 {
-	if(f==-1)
+	if (f == -1)
 		printf("\nempty\n");
-	else if(f==r)
-		f=r=-1;
-	else if(f==size-1)
-		f=0;
+	else if (f == r)
+		f = r = -1;
+	else if (f == size - 1)
+		f = 0;
 	else
 		f++;
 }
 void display()
 {
-	if(f==-1)
+	if (f == -1)
 		printf("empty");
-	else if(f<=r)
+	else if (f <= r)
 	{
-		for(i=f;i<=r;i++)
-			printf(" %d ",arr[i]);
+		for (i = f; i <= r; i++)
+			printf(" %d ", arr[i]);
 	}
 	else
 	{
-		for(i=f;i<=size-1;i++)
-			printf(" %d ",arr[i]);
-		for(i=0;i<=r;i++)
-			printf(" %d ",arr[i]);
+		for (i = f; i <= size - 1; i++)
+			printf(" %d ", arr[i]);
+		for (i = 0; i <= r; i++)
+			printf(" %d ", arr[i]);
 	}
 }
 void main()
@@ -50,18 +52,27 @@ void main()
 	int ch;
 	clrscr();
 
-	while(1)
+	while (1)
 	{
 		printf("\n0 exit  1 insert 2 delete 3 display : ");
-		scanf("%d",&ch);
+		scanf("%d", &ch);
 
-		switch(ch)
+		switch (ch)
 		{
-			case 0: exit(0);
-			case 1: insert(); break;
-			case 2: delete(); break;
-			case 3: display(); break;
-			default: printf("\ninvalid"); break;
+		case 0:
+			exit(0);
+		case 1:
+			insert();
+			break;
+		case 2:
+			delete ();
+			break;
+		case 3:
+			display();
+			break;
+		default:
+			printf("\ninvalid");
+			break;
 		}
 	}
 }
