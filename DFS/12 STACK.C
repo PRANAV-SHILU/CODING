ec40b379f8ac09902992ec333d79196364e400d7
PRANAV SHILU <pranavshilu1811@gmail.com>
2026-04-03 09:41:12 +0530

    TOS, push, pop, peep explaination

--- DFS/12 STACK.C
+++ DFS/12 STACK.C
@@ -1,44 +1,48 @@
-#include<stdio.h>
-#include<conio.h>
-#include<process.h>
-int tos=-1,arr[5],size=5;
-void push()
+#include <stdio.h>
+#include <conio.h>
+#include <process.h>
+int tos = -1, arr[5], size = 5;
+// tos=top of stack
+// push to insert - tos++
+// pop to delete - tos--
+// peep to see last inserted and last deleted
+void push() // insert
 {
-	if(tos==size-1)
+	if (tos == size - 1)
 		printf("\nstack overflow\n");
 	else
 	{
 		tos++;
-		scanf("%d",&arr[tos]);
+		scanf("%d", &arr[tos]);
 	}
 }
-void pop()
+void pop() // delete
 {
-	if(tos==-1)
+	if (tos == -1)
 		printf("\nstack underflow\n");
 	else
 		tos--;
 }
-void peep()
+void peep() // see last inserted and last deleted
 {
-	if(tos==-1)
-		printf("\nstack unferflow\n");
+	if (tos == -1)
+		printf("\nstack underflow\n");
 	else
 	{
-		printf("\nLast inserted : %d ",arr[tos]);   //last inserted
-		printf("\nLast deleted : %d\n",arr[tos+1]);  //last deleted
+		printf("\nLast inserted : %d ", arr[tos]);	   // last inserted
+		printf("\nLast deleted : %d\n", arr[tos + 1]); // last deleted
 	}
 }
-void display()
+void display() // display reverse
 {
-	if(tos==-1)
+	if (tos == -1)
 		printf("\nstack underflow\n");
 	else
 	{
 		int i;
 		printf("\ndisplay reverse : ");
-		for(i=tos;i>=0;i--)
-			printf("%d | ",arr[i]);
+		for (i = tos; i >= 0; i--)
+			printf("%d | ", arr[i]);
 	}
 }
 void main()
@@ -46,30 +50,30 @@ void main()
 	int ch;
 	clrscr();
 
-	while(1)
+	while (1)
 	{
 		printf("\n1 push 2 pop 3 peep 4 display reverse 0 exit : ");
-		scanf("%d",&ch);
+		scanf("%d", &ch);
 
-		switch(ch)
+		switch (ch)
 		{
-			case 0:
-				exit(0);
-			case 1:
-				push();
-				break;
-			case 2:
-				pop();
-				break;
-			case 3:
-				peep();
-				break;
-			case 4:
-				display();
-				break;
-			default:
-				 printf("\nInvalid chice\n");
-				 break;
+		case 0:
+			exit(0);
+		case 1:
+			push();
+			break;
+		case 2:
+			pop();
+			break;
+		case 3:
+			peep();
+			break;
+		case 4:
+			display();
+			break;
+		default:
+			printf("\nInvalid chice\n");
+			break;
 		}
 	}
 }
