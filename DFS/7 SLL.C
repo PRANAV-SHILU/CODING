d616907b5f047cd08813ca0e43e8ad98876fe263
PRANAV SHILU <pranavshilu1811@gmail.com>
2026-04-05 09:47:08 +0530

    Enhanced code readability

--- DFS/7 SLL.C
+++ DFS/7 SLL.C
@@ -1,161 +1,158 @@
-#include<stdio.h>
-#include<conio.h>
-#include<process.h>
+#include <stdio.h>
+#include <conio.h>
+#include <process.h>
 struct stud
 {
 	int roll;
 	struct stud *next;
 };
-struct stud *head=NULL,*new1,*temp,*temp1;
-int node=0,node_no;
+struct stud *head = NULL, *new1, *temp, *temp1;
+int node = 0, node_no;
 
 struct stud *create()
 {
-	new1=(struct stud *)malloc(sizeof(struct stud));
+	new1 = (struct stud *)malloc(sizeof(struct stud));
 
 	printf("\nEnter roll no : ");
-	scanf("%d",&new1->roll);
+	scanf("%d", &new1->roll);
 
-	new1->next=NULL;
+	new1->next = NULL;
 
 	return new1;
 }
 void ins()
 {
-	if(head==NULL)
-	{
-		head=create();
-	}
+	if (head == NULL)
+		head = create();
 	else
 	{
-	       temp=head;
-	       while(temp->next!=NULL)
-	       {
-			temp=temp->next;
-	       }
-	       temp->next=create();
+		temp = head;
+		while (temp->next != NULL)
+			temp = temp->next;
+
+		temp->next = create();
 	}
 	node++;
 }
 void ins_first()
 {
-	temp=create();
-	temp->next=head;
-	head=temp;
+	temp = create();
+	temp->next = head;
+	head = temp;
 	node++;
 }
 void ins_spe_before()
 {
-	int cnt=1;
+	int cnt = 1;
 	printf("\nENter node no : ");
-	scanf("%d",&node_no);
+	scanf("%d", &node_no);
 
-	if(head==NULL)
+	if (head == NULL)
 	{
 		textcolor(RED);
 		cprintf("\nNode is Empty !\n");
 	}
-	else if(node_no>node || node_no<2)
+	else if (node_no > node || node_no < 2)
 	{
 		textcolor(RED);
 		cprintf("\nInvalid node no !\n");
 	}
 	else
 	{
-		temp=head;
-		while(node_no!=cnt)
+		temp = head;
+		while (node_no != cnt)
 		{
-			temp1=temp;
-			temp=temp->next;
+			temp1 = temp;
+			temp = temp->next;
 			cnt++;
 		}
-		temp1->next=create();
-		new1->next=temp;
+		temp1->next = create();
+		new1->next = temp;
 
 		node++;
 	}
 }
 void ins_spe_after()
 {
-	int cnt=1;
+	int cnt = 1;
 	printf("\nENter node no : ");
-	scanf("%d",&node_no);
+	scanf("%d", &node_no);
 
-	if(head==NULL)
+	if (head == NULL)
 	{
 		textcolor(RED);
 		cprintf("\nNode is Empty !\n");
 	}
-	else if(node_no>node || node_no<1)
+	else if (node_no > node || node_no < 1)
 	{
 		textcolor(RED);
 		cprintf("\nInvalid node no !\n");
 	}
 	else
 	{
-		while(node_no!=cnt)
+		while (node_no != cnt)
 		{
-			temp=temp->next;
+			temp = temp->next;
 			cnt++;
 		}
-		temp1=create();
-		temp1->next=temp->next;
-		temp->next=temp1;
+		temp1 = create();
+		temp1->next = temp->next;
+		temp->next = temp1;
 		node++;
 	}
 }
 void dis()
 {
-	if(head==NULL)
+	if (head == NULL)
 	{
 		textcolor(RED);
 		cprintf("\nNode is Empty !\n");
 	}
 	else
 	{
-		temp=head;
+		temp = head;
 		textcolor(GREEN);
 		cprintf("\nDISPLAY");
-		while(temp!=NULL)
+		while (temp != NULL)
 		{
-			printf("\nRoll no : %d",temp->roll);
-			temp=temp->next;
+			printf("\nRoll no : %d", temp->roll);
+			temp = temp->next;
 		}
 		printf("\n");
 		textcolor(YELLOW);
-		cprintf("TOTAL NODE = %d\n",node);
+		cprintf("TOTAL NODE = %d\n", node);
 	}
 }
 void dis_spe()
 {
-	int cnt=1;
+	int cnt = 1;
 	printf("\nENter node no : ");
-	scanf("%d",&node_no);
+	scanf("%d", &node_no);
 
-	if(head==NULL)
+	if (head == NULL)
 	{
 		textcolor(RED);
 		cprintf("\nNode is Empty !\n");
 	}
-	else if(node_no>node || node_no<1)
+	else if (node_no > node || node_no < 1)
 	{
 		textcolor(RED);
 		cprintf("\nInvalid node no !\n");
 	}
 	else
 	{
-		temp=head;
-		while(node_no!=cnt)
+		temp = head;
+		while (node_no != cnt)
 		{
-			temp=temp->next;
+			temp = temp->next;
 			cnt++;
 		}
-		printf("\nRoll no : %d",temp->roll);
+		printf("\nRoll no : %d", temp->roll);
 	}
 }
 void dis_first()
 {
-	if(head==NULL)
+	if (head == NULL)
 	{
 		textcolor(RED);
 		cprintf("\nNode is Empty !\n");
@@ -163,39 +160,39 @@ void dis_first()
 	else
 	{
 		textcolor(GREEN);
-		cprintf("\nFirst Roll no = %d\n",head->roll);
+		cprintf("\nFirst Roll no = %d\n", head->roll);
 	}
 }
 void dislast()
 {
-	if(head==NULL)
+	if (head == NULL)
 	{
 		textcolor(RED);
 		cprintf("\nNode is Empty !\n");
 	}
 	else
 	{
-		temp=head;
+		temp = head;
 
-		while(temp->next!=NULL)
+		while (temp->next != NULL)
 		{
-			temp=temp->next;
+			temp = temp->next;
 		}
 		textcolor(GREEN);
-		cprintf("Last Roll no = %d\n",temp->roll);
+		cprintf("Last Roll no = %d\n", temp->roll);
 	}
 }
 void delfirst()
 {
-	if(head==NULL)
+	if (head == NULL)
 	{
 		textcolor(RED);
 		cprintf("\nNode is Empty !\n");
 	}
 	else
 	{
-		temp=head;
-		head=head->next;
+		temp = head;
+		head = head->next;
 		free(temp);
 		textcolor(RED);
 		cprintf("\nNODE DELETED!\n");
@@ -204,32 +201,32 @@ void delfirst()
 }
 void del_spe()
 {
-	int cnt=1;
+	int cnt = 1;
 	printf("\nENter node no : ");
-	scanf("%d",&node_no);
+	scanf("%d", &node_no);
 
-	if(head==NULL)
+	if (head == NULL)
 	{
 		textcolor(RED);
 		cprintf("\nNode is Empty !\n");
 	}
-	else if(node_no>node || node_no<node)
+	else if (node_no > node || node_no < node)
 	{
 		textcolor(RED);
 		cprintf("\nInvalid node no !\n");
 	}
-	else if(node_no==1)
+	else if (node_no == 1)
 		delfirst();
 	else
 	{
-		temp=head;
-		while(node_no!=cnt)
+		temp = head;
+		while (node_no != cnt)
 		{
-			temp1=temp;
-			temp=temp->next;
+			temp1 = temp;
+			temp = temp->next;
 			cnt++;
 		}
-		temp1->next=temp->next;
+		temp1->next = temp->next;
 		free(temp);
 
 		textcolor(RED);
@@ -239,33 +236,33 @@ void del_spe()
 }
 void dellast()
 {
-	if(head==NULL)
+	if (head == NULL)
 	{
 		textcolor(RED);
 		cprintf("\nNode is Empty !\n");
 	}
-	else if(head->next==NULL)
+	else if (head->next == NULL)
 		delfirst();
 	else
 	{
-		//logic 1
-	 /*	struct stud *temp1;
-		temp=head;
-		while(temp->next!=NULL)
-		{
-			temp1=temp;
-			temp=temp->next;
-		}
-		free(temp->next);
-		temp1->next=NULL;      */
+		// logic 1
+		/*	struct stud *temp1;
+		   temp=head;
+		   while(temp->next!=NULL)
+		   {
+			   temp1=temp;
+			   temp=temp->next;
+		   }
+		   free(temp->next);
+		   temp1->next=NULL;      */
 
-		//logic 2
-		temp=head;
-		while(temp->next->next!=NULL)
-			temp=temp->next;
+		// logic 2
+		temp = head;
+		while (temp->next->next != NULL)
+			temp = temp->next;
 
 		free(temp->next);
-		temp->next=NULL;
+		temp->next = NULL;
 
 		textcolor(RED);
 		cprintf("\nNODE DELETED!");
@@ -274,27 +271,27 @@ void dellast()
 }
 void update()
 {
-	int rno,isfound=1;
+	int rno, isfound = 1;
 	printf("\nEnter roll no : ");
-	scanf("%d",&rno);
+	scanf("%d", &rno);
 
-	if(head==NULL)
+	if (head == NULL)
 	{
 		textcolor(RED);
 		cprintf("\nNode is Empty !\n");
 	}
 	else
 	{
-		temp=head;
+		temp = head;
 
-		while(rno!=temp->roll)
+		while (rno != temp->roll)
 		{
-			temp=temp->next;
-			if(temp==NULL)
+			temp = temp->next;
+			if (temp == NULL)
 				break;
 		}
 		printf("\nEnter new roll no : ");
-		scanf("%d",&temp->roll);
+		scanf("%d", &temp->roll);
 	}
 }
 void main()
@@ -302,7 +299,7 @@ void main()
 	int ch;
 	clrscr();
 
-	while(1)
+	while (1)
 	{
 		printf("\n0 Exit ");
 		printf("\n1 Insert ");
@@ -318,55 +315,54 @@ void main()
 		printf("\n7 Delete specific");
 		printf("\n10 Update record");
 		printf("\nEnter your choice : ");
-		scanf("%d",&ch);
+		scanf("%d", &ch);
 
-		switch(ch)
+		switch (ch)
 		{
-			case 0:
-				exit(0);
-				break;
-			case 1:
-				ins();
-				break;
-			case 11:
-				ins_first();
-				break;
-			case 8:
-				ins_spe_before();
-				break;
-			case 9:
-				ins_spe_after();
-				break;
-			case 2:
-				dis();
-				break;
-			case 22:
-				dis_spe();
-				break;
-			case 3:
-				dis_first();
-				break;
-			case 4:
-				textcolor(GREEN);
-				dislast();
-				break;
-			case 5:
-				delfirst();
-				break;
-			case 6:
-				dellast();
-				break;
-			case 7:
-				del_spe();
-				break;
-			case 10:
-				update();
-				break;
-			default:
-				textcolor(RED);
-				cprintf("\nINVALID CHOICE !\n");
-				break;
-
+		case 0:
+			exit(0);
+			break;
+		case 1:
+			ins();
+			break;
+		case 11:
+			ins_first();
+			break;
+		case 8:
+			ins_spe_before();
+			break;
+		case 9:
+			ins_spe_after();
+			break;
+		case 2:
+			dis();
+			break;
+		case 22:
+			dis_spe();
+			break;
+		case 3:
+			dis_first();
+			break;
+		case 4:
+			textcolor(GREEN);
+			dislast();
+			break;
+		case 5:
+			delfirst();
+			break;
+		case 6:
+			dellast();
+			break;
+		case 7:
+			del_spe();
+			break;
+		case 10:
+			update();
+			break;
+		default:
+			textcolor(RED);
+			cprintf("\nINVALID CHOICE !\n");
+			break;
 		}
 	}
 }
--- DFS/9 DLL.C
+++ DFS/9 DLL.C
@@ -1,67 +1,66 @@
-#include<stdio.h>
-#include<conio.h>
-#include<process.h>
+#include <stdio.h>
+#include <conio.h>
+#include <process.h>
 struct stud
 {
 	int roll;
-	struct stud *next,*prev;
+	struct stud *next, *prev;
 };
-struct stud *head=NULL,*new1,*temp,*temp1;
-int node=0;
+struct stud *head = NULL, *new1, *temp, *temp1;
+int node = 0;
 
 struct stud *create()
 {
-	new1=(struct stud *)malloc(sizeof(struct stud));
+	new1 = (struct stud *)malloc(sizeof(struct stud));
 
 	printf("\nEnter roll no : ");
-	scanf("%d",&new1->roll);
+	scanf("%d", &new1->roll);
 
-	new1->next=NULL;
-	new1->prev=NULL;
+	new1->next = NULL;
+	new1->prev = NULL;
 
 	return new1;
 }
 void ins()
 {
-	if(head==NULL)
-		head=create();
+	if (head == NULL)
+		head = create();
 	else
 	{
-	       temp=head;
-	       while(temp->next!=NULL)
-	       {
-			temp=temp->next;
-	       }
-	       temp->next=create();
-	       new1->prev=temp;
+		temp = head;
+		while (temp->next != NULL)
+			temp = temp->next;
+
+		temp->next = create();
+		new1->prev = temp;
 	}
 	node++;
 }
 void dis_rev()
 {
-	if(head==NULL)
+	if (head == NULL)
 	{
 		textcolor(RED);
 		cprintf("\nNode is Empty !\n");
 	}
 	else
 	{
-		temp=head;
+		temp = head;
 		textcolor(GREEN);
 		cprintf("\nDISPLAY");
 
-		while(temp->next!=NULL)
-			temp=temp->next;
+		while (temp->next != NULL)
+			temp = temp->next;
 
-		while(temp!=NULL)
+		while (temp != NULL)
 		{
-			printf("\nRoll no : %d",temp->roll);
-			temp=temp->prev;
+			printf("\nRoll no : %d", temp->roll);
+			temp = temp->prev;
 		}
 
 		printf("\n");
 		textcolor(YELLOW);
-		cprintf("TOTAL NODE = %d\n",node);
+		cprintf("TOTAL NODE = %d\n", node);
 	}
 }
 void main()
@@ -69,30 +68,29 @@ void main()
 	int ch;
 	clrscr();
 
-	while(1)
+	while (1)
 	{
 		printf("\n0 Exit ");
 		printf("\n1 Insert ");
 		printf("\n2 Display Reverse ");
 		printf("\nEnter your choice : ");
-		scanf("%d",&ch);
+		scanf("%d", &ch);
 
-		switch(ch)
+		switch (ch)
 		{
-			case 0:
-				exit(0);
-				break;
-			case 1:
-				ins();
-				break;
-			case 2:
-				dis_rev();
-				break;
-			default:
-				textcolor(RED);
-				cprintf("\nINVALID CHOICE !\n");
-				break;
-
+		case 0:
+			exit(0);
+			break;
+		case 1:
+			ins();
+			break;
+		case 2:
+			dis_rev();
+			break;
+		default:
+			textcolor(RED);
+			cprintf("\nINVALID CHOICE !\n");
+			break;
 		}
 	}
 }
