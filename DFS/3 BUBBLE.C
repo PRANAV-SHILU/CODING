65373bc8178c388e686b8ee886d31d1ab9b68fb9
PRANAV SHILU <pranavshilu1811@gmail.com>
2026-04-09 10:32:40 +0530

    Bubble sort and insertition sort code enhancement

--- DFS/3 BUBBLE.C
+++ DFS/3 BUBBLE.C
@@ -1,35 +1,36 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <conio.h>
 void main()
 {
-	int i,j,temp,a[5];
+	int i, j, temp, a[5];
 	clrscr();
 
-	for(i=0;i<5;i++)
+	for (i = 0; i < 5; i++)
 	{
-		printf("Enter [%d] : ",i+1);
-		scanf("%d",&a[i]);
+		printf("Enter [%d] : ", i + 1);
+		scanf("%d", &a[i]);
 	}
 
-	for(i=0;i<5;i++)
+	for (i = 0; i < 5; i++)
 	{
-		for(j=0;j<4;j++)
+		for (j = 0; j < 4; j++)
 		{
-			if(a[j]>a[j+1])
+			if (a[j] > a[j + 1])
 			{
-				temp=a[j];
-				a[j]=a[j+1];
-				a[j+1]=temp;
+				// swapping logic
+				temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
 			}
 		}
 	}
 
 	textcolor(RED);
 	cprintf("\n\nBUBBLE SORT :  ");
-	for(i=0;i<5;i++)
+	for (i = 0; i < 5; i++)
 	{
 		textcolor(GREEN);
-	       cprintf("%d  ",a[i]);
+		cprintf("%d  ", a[i]);
 	}
 	getch();
 }
--- DFS/5 INSERTION.C
+++ DFS/5 INSERTION.C
@@ -1,34 +1,33 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <conio.h>
 void main()
 {
-	int i,j,temp,a[5];
+	int i, j, temp, a[5];
 	clrscr();
 
-	for(i=0;i<5;i++)
+	for (i = 0; i < 5; i++)
 	{
-		printf("\nEnter [%d] : ",i+1);
-		scanf("%d",&a[i]);
+		printf("\nEnter [%d] : ", i + 1);
+		scanf("%d", &a[i]);
 
-		temp=a[i];
-		for(j=i-1;j>=0;j--)
+		temp = a[i];
+		for (j = i - 1; j >= 0; j--)
 		{
-			if(a[j]>temp)
+			if (a[j] > temp)
 			{
-				a[j+1]=a[j];
-				a[j]=temp;
+				a[j + 1] = a[j];
+				a[j] = temp;
 			}
 		}
 
 		printf("\t\t");
 		textcolor(RED);
 		cprintf("INSERTION SORT : ");
-		for(j=0;j<=i;j++)
+		for (j = 0; j <= i; j++)
 		{
 			textcolor(GREEN);
-			cprintf("%d  ",a[j]);
+			cprintf("%d  ", a[j]);
 		}
-
 	}
 	getch();
 }
